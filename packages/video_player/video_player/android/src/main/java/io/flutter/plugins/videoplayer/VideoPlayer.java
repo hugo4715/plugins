c8c0306944b68ba94487ad868bbfc354be1f62a4
hugo4715 <deniauhugo@gmail.com>
2022-03-13 20:43:38 +0100

    Changed chewie user agent to a fake Mozilla user agent

--- packages/video_player/video_player/android/src/main/java/io/flutter/plugins/videoplayer/VideoPlayer.java
+++ packages/video_player/video_player/android/src/main/java/io/flutter/plugins/videoplayer/VideoPlayer.java
@@ -78,7 +78,7 @@ final class VideoPlayer {
     if (isHTTP(uri)) {
       DefaultHttpDataSource.Factory httpDataSourceFactory =
           new DefaultHttpDataSource.Factory()
-              .setUserAgent("ExoPlayer")
+              .setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36")
               .setAllowCrossProtocolRedirects(true);
 
       if (httpHeaders != null && !httpHeaders.isEmpty()) {
